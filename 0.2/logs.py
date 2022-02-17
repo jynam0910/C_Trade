40cc8ab3576c38791511a95cdd0e07aba934687c
youngjun <dragon644@naver.com>
2022-02-17 13:26:04 +0900

    로그 타임스템프 12시간제->24시간제

--- 0.2/logs.py
+++ 0.2/logs.py
@@ -4,13 +4,13 @@ import os
 #TODO level 'fetal' 추가
 #TODO level별 글씨색 다르게 설정
 
-time.strftime('%Y-%m-%d %I:%M:%S', time.localtime())
-starttime = time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(time.time()))
+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+starttime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 os.mkdir(starttime)
 
 def start_log():
     log = time.time()
-    logtime = time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(log))
+    logtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(log))
     normal = open(starttime + "/[normal]" + logtime + ".txt", 'a')
     critical = open(starttime + "/[critical]" + logtime + ".txt", 'a')
     normal.close()
@@ -18,20 +18,20 @@ def start_log():
     return log
 
 def logger(log,level,message):
-    logtime = time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(log))
+    logtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(log))
     normal = open(starttime + "/[normal]" + logtime + ".txt", 'a')
     critical = open(starttime + "/[critical]" + logtime + ".txt", 'a')
     if time.time() - log > 3600*3:
         normal.close()
         critical.close()
         log = time.time()
-        logtime = time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(log))
+        logtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(log))
         normal = open(starttime + "/[normal]" + logtime + ".txt", 'a')
         critical = open(starttime + "/[critical]" + logtime + ".txt", 'a')
     if level == 'critical':
-        critical.write(time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(time.time())) + '  ' + str(message) + '\n')
-    normal.write(time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(time.time())) + '  ' + str(message) + '\n')
-    print(time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(time.time())) + '  ' + str(message))
+        critical.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '  ' + str(message) + '\n')
+    normal.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '  ' + str(message) + '\n')
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '  ' + str(message))
     normal.close()
     critical.close()
     return log
